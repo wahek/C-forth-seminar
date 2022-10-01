a2e724c8b4d157874860f7346abe0298152f3ded
wahek <wahek1999@mail.ru>
2022-10-01 21:24:34 +0300

    Первая задача

--- Program.cs
+++ Program.cs
@@ -6,6 +6,21 @@
 
 // 2, 4 -> 16
 
+Console.WriteLine("Введите первоe и второе число:");
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+Console.Write($"Число {A} в степени {B} = {sqr(A)}");
+
+int sqr(int a){
+    int A = a;
+    for (int i = 2; i <= B; i++){
+        A = A * a;
+    }
+    return A;
+}
+    
+
+
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 // 452 -> 11
@@ -13,6 +28,20 @@
 // 82 -> 10
 
 // 9012 -> 12
+
+// Console.WriteLine("Введите число:");
+// int num = int.Parse(Console.ReadLine()!);
+
+// int doSum(int number){
+//     int sum = 0;
+//     for (int postNum = number;postNum > 0; postNum%10){
+//         int element = num % 10;
+//         sum += element;
+//     }
+
+//     }
+
+
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
 
